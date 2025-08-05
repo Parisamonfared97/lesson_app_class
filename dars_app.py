d3d1855d6852c6420c7368758d08f64fa4206786
Student <parisamonfared97@gmail.com>
2025-08-05 17:32:45 +0330

    table added

--- dars_app.py
+++ dars_app.py
@@ -10,7 +10,8 @@ def edit_click():
     pass
 def remove_click():
     pass
-
+def select_class(event):
+    pass
 
 
 window = Tk()
@@ -49,11 +50,33 @@ Entry(window, textvariable=unit).place(x=80, y=220)
 
 #Buttons
 Button(window, text="Ok", command=ok_click).place(x=80, y=300)
-Button(window, text="Ok", command=edit_click).place(x=80, y=300)
-Button(window, text="Ok", command=remove_click).place(x=80, y=300)
+Button(window, text="Edit", command=edit_click).place(x=110, y=300)
+Button(window, text="Remove", command=remove_click).place(x=150, y=300)
+
+#search by class title
+title=StringVar()
+Label(window, text="Search By Title").place(x=200, y=20)
+Entry(window, textvariable=title).place(x=290, y=20)
 
 
+#search by teacher
+teacher=StringVar()
+Label(window, text="Search By Teacher").place(x=400, y=20)
+Entry(window, textvariable=title).place(x=500, y=20)
 
+#Table
+table=ttk.Treeview(window,height=35,columns=("Title","Teacher","Class","Unit"),show="headings")
+table.column("Title", width=100)
+table.column("Teacher", width=100)
+table.column("Class", width=100)
+table.column("Unit", width=100)
 
+table.heading("Title", text="Title")
+table.heading("Teacher", text="Teacher")
+table.heading("Class", text="Class")
+table.heading("Unit", text="Unit")
+
+table.bind("<<TreeviewSelect>>", select_class)
+table.place(x=250,y=80)
 
 window.mainloop()
